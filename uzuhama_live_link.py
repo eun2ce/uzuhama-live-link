26e6a9b79debaa9220442222247c3466973e00ab
eun2ce <joeun2ce@gmail.com>
2025-02-13 13:51:18 +0900

    init

---
+++ uzuhama_live_link.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+API_KEY = os.getenv('YOUTUBE_API_KEY')
+CHANNEL_ID = os.getenv('YOUTUBE_CHANNEL_ID')
+URL = f'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={CHANNEL_ID}&eventType=live&type=video&key={API_KEY}'
+
+# Markdown 파일 경로
+markdown_file = "readme.md"
+
+response = requests.get(URL)
+data = response.json()
+
+if 'items' in data and len(data['items']) > 0:
+    video_id = data['items'][0]['id']['videoId']
+    live_stream_url = f'https://www.youtube.com/watch?v={video_id}'
+
+    if data['items'][0]['snippet']['liveBroadcastContent'] == 'live':
+        with open(markdown_file, "a") as f:
+            f.write(f"{live_stream_url}\n")
+else:
+    print("No live stream currently.")
