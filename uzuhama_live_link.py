4c4fa227df6bc15edba5a88458d51e93afd5046b
eun2ce <joeun2ce@gmail.com>
2025-02-13 17:05:47 +0900

    Update uzuhama_live_link.py

--- uzuhama_live_link.py
+++ uzuhama_live_link.py
@@ -6,8 +6,9 @@ API_KEY = os.getenv('YOUTUBE_API_KEY')
 CHANNEL_ID = os.getenv('YOUTUBE_CHANNEL_ID')
 URL = f'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={CHANNEL_ID}&eventType=live&type=video&key={API_KEY}'
 
-# Markdown 파일 경로
 markdown_file = "readme.md"
+
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 response = requests.get(URL)
 data = response.json()
@@ -17,8 +18,24 @@ if 'items' in data and len(data['items']) > 0:
     live_stream_url = f'https://www.youtube.com/watch?v={video_id}'
 
     if data['items'][0]['snippet']['liveBroadcastContent'] == 'live':
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        with open(markdown_file, "a") as f:
-            f.write(f"- {current_datetime} [{live_stream_url}]({live_stream_url})\n")
+        with open(markdown_file, "r") as f:
+            content = f.readlines()
+
+        link_found = False
+        for i, line in enumerate(content):
+            if live_stream_url in line:
+                existing_times = line.split(" ")[0]  # 날짜 부분만 추출
+                new_line = f"{existing_times}, {current_time} {live_stream_url}\n"
+                content[i] = new_line
+                link_found = True
+                break
+
+        if not link_found:
+            content.append(f"{current_time} {live_stream_url}\n")
+
+        # 파일에 다시 저장
+        with open(markdown_file, "w") as f:
+            f.writelines(content)
+
 else:
     print("No live stream currently.")
